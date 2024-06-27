8f8ca2f0ced50af56ee8e06378ba061f0741862b
Thiago Batista <thiagobatista15390@gmail.com>
2024-06-27 10:39:47 -0300

    criando a contagem regressiva

--- src/script/main.js
+++ src/script/main.js
@@ -1,1 +1,27 @@
-console.log("olá");
+AOS.init();
+
+const dataDoEvent = new Date("Dec 30, 2024 19:00:00");
+const timeStampDoEvento = dataDoEvent.getTime();
+
+const contaAsHoras = setInterval(function() {
+    const agora = new Date();
+    const timeStampAtual = agora.getTime();
+
+    const distanciaAteOEvento = timeStampDoEvento - timeStampAtual;
+
+    const diasEmMs = 1000 * 60 * 60 *24;
+    const horaEmMs = 1000 * 60 * 60;
+    const minutoEmMs = 1000 *60;
+
+    const diasAteOEvento = Math.floor(distanciaAteOEvento / diasEmMs);
+    const horasAteOEvento = Math.floor((distanciaAteOEvento % diasEmMs) / horaEmMs);
+    const minutosAteOevento = Math.floor((distanciaAteOEvento % horaEmMs) / minutoEmMs);
+    const segundosAteOEvento = Math.floor((distanciaAteOEvento % minutoEmMs) / 1000);
+
+    document.getElementById("contador").innerHTML = `${diasAteOEvento}d ${horasAteOEvento}h ${minutosAteOevento}m ${segundosAteOEvento}s`;
+
+    if(distanciaAteOEvento < 0 ){
+        clearInterval(contaAsHoras);
+        document.getElementById("contador").innerHTML = "Evento expirado"
+    }
+},1000);
